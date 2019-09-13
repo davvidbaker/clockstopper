8892ecb6112767d5c7c58feff5989e9049c36b95
davvidbaker <davvidbaker@gmail.com>
2019-09-13 10:07:44 -0600

    change icon, background gradient

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 console.log(
-  '%c     %c    %c    %c   PRESS F5 or ∂ (ALT + D) to debug while dev tools is open   %c    %c    %c     ',
-  'background-color: orange;',
-  'background-color: gold;',
-  'background-color: yellow;',
-  'background-color: lightyellow; font-weight: bold',
-  'background-color: yellow;',
-  'background-color: gold;',
-  'background-color: orange;',
+  '%c ⏱️ PRESS F5 or ∂ (ALT + D) to debug while dev tools is open ⏱️ ',
+  'padding: 0 48px; font-weight: bold; background: linear-gradient(90deg, gold, lightyellow 8%, lightyellow 92%, gold);',
 );
 
 document.addEventListener('keydown', e => {
