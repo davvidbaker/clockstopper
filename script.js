d219c683567fa768c187529e0f0e5bb5177d0a38
davvidbaker <davvidbaker@gmail.com>
2019-09-13 15:44:40 -0600

    hacky iframe detection

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 console.log(
   '%c ⏱️ PRESS F5 or ∂ (ALT + D) to debug while dev tools is open ⏱️ ',
-  'padding: 0 48px; font-weight: bold; background: linear-gradient(90deg, gold, lightyellow 8%, lightyellow 92%, gold);',
+  'padding: 0 48px; font-weight: bold; background: linear-gradient(90deg, gold, lightyellow 8%, lightyellow 92%, gold);'
 );
-
-document.addEventListener('keydown', e => {
+const listener = e => {
   // alt + D (at least on my computer)
   if (e.code === 'F5' || e.key === '∂') {
     debugger;
   }
-});
+};
+
+document.addEventListener('keydown', listener);
+
+/* 💁 should use a mutation observer to detect iframes, but it wasn't working in the storybook I was testing on... unsure why... */
+setTimeout(() => {
+  for (let i of document.querySelectorAll('iframe')) {
+    i.contentDocument.addEventListener('keydown', listener);
+  }
+}, 3000);
